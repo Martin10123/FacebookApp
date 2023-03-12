147b9c3257bd24476ccfac9765a15ee32231685e
Martin10123 <martinsimarra4@gmail.com>
2023-03-11 19:25:48 -0500

    Mostrar detalles en la pagina del perfil principal

--- src/ProfilePage/page/DetailsUser.jsx
+++ src/ProfilePage/page/DetailsUser.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { EditProfile } from "../components";
+import { EditDetailsItem, EditProfile } from "../components";
 
 import styles from "./profile.module.css";
 
@@ -16,14 +16,8 @@ export const DetailsUser = () => {
         <h3 className={styles.title_details}>Detalles</h3>
 
         <ul className={styles.profile__list_details}>
-          <li className={styles.profile__list_item}>
-            <i className="fa-solid fa-briefcase"></i>
-            No trabajo mis padres me mantiene
-          </li>
-          <li className={styles.profile__list_item}>
-            <i className="fa-solid fa-briefcase"></i>
-            No trabajo mis padres me mantiene
-          </li>
+          <EditDetailsItem />
+
           <button
             className={styles.profile__edit_public_details}
             onClick={() => setOpenEditProfile(true)}
